c512fcdff2d64013b16c04546dfb966d73eebeed
georgel115 <georgel115@nycstudents.net>
2025-11-03 01:58:52 -0500

    APCalendar

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//public class Main
+//{
+//    static void main()
+//    {
+//        System.out.println(APCalendar.firstDayOfYear(2025));
+//        System.out.println(APCalendar.dayOfYear(11, 3, 2025));
+//        System.out.println(340 % 7);
+//        System.out.println(APCalendar.dayOfWeek(11, 3, 2025));
+//    }
+//}
