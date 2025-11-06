1258711aef6020193775ff374a7d3803ddcc232d
GeorgeLin <georgel115@nycstudents.net>
2025-11-06 11:26:15 -0500

    cheese

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(APCalendar.firstDayOfYear(2005));
-        System.out.println(APCalendar.dayOfYear(2, 10, 2000));
-        System.out.println(44 % 7);
-        System.out.println(APCalendar.dayOfWeek(2, 10, 2000));
+        System.out.println(APCalendar.dayOfWeek(1, 5, 2019));
+        System.out.println(APCalendar.dayOfWeek(1, 10, 2019));
+        System.out.println(APCalendar.firstDayOfYear(2019));
+        System.out.println(APCalendar.dayOfYear(3, 1, 2017));
+        System.out.println(APCalendar.dayOfYear(3, 1, 2016));
     }
 }
